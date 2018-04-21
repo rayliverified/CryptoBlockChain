c6b6f5d3f32dd6083bb2cdf4f1d38ed027aad277
AlexanderCrane <InnovateTheWorld@yahoo.com>
2018-04-21 16:47:11 -0500

    Alex Branch First Commit

--- app/src/main/java/stream/crypto/MainActivity.java
+++ app/src/main/java/stream/crypto/MainActivity.java
@@ -1,9 +1,11 @@
 package stream.crypto;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,13 @@ public class MainActivity extends AppCompatActivity {
     TextView mBtn5;
     TextView mBtn6;
 
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplication().getApplicationContext();
 
         mBtn1 = findViewById(R.id.btn_1);
         mBtn2 = findViewById(R.id.btn_2);
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         mBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(mContext, "Hi!", Toast.LENGTH_SHORT).show();
             }
         });
         mBtn2.setOnClickListener(new View.OnClickListener() {
