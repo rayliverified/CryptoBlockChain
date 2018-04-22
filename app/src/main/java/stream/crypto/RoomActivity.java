e36a16e7b8e874a4dc4aaa58a3a5f3e93625f905
Ray Li <ray.li.verified@gmail.com>
2018-04-21 21:14:12 -0500

    Create Room Activity Layout
    *Create Room Activity Layout.
    *Create health bar and placeholder ImageViews.

--- app/src/main/java/stream/crypto/RoomActivity.java
+++ app/src/main/java/stream/crypto/RoomActivity.java
@@ -1,22 +1,20 @@
 package stream.crypto;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.applovin.adview.AppLovinInterstitialAd;
-import com.applovin.adview.AppLovinInterstitialAdDialog;
+import com.adcolony.sdk.AdColony;
+import com.adcolony.sdk.AdColonyInterstitial;
+import com.adcolony.sdk.AdColonyInterstitialListener;
 import com.applovin.sdk.AppLovinAd;
-import com.applovin.sdk.AppLovinAdClickListener;
-import com.applovin.sdk.AppLovinAdDisplayListener;
 import com.applovin.sdk.AppLovinAdLoadListener;
 import com.applovin.sdk.AppLovinAdSize;
-import com.applovin.sdk.AppLovinAdVideoPlaybackListener;
+import com.applovin.sdk.AppLovinSdk;
+import com.bumptech.glide.Glide;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
@@ -24,27 +22,20 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
-import com.applovin.sdk.AppLovinSdk;
-import com.adcolony.sdk.AdColony;
-import com.adcolony.sdk.AdColonyInterstitial;
-import com.adcolony.sdk.AdColonyInterstitialListener;
+import com.moos.library.HorizontalProgressView;
 
-public class MainActivity extends AppCompatActivity {
+public class RoomActivity extends AppCompatActivity {
 
-    TextView mBtn1;
-    TextView mBtn2;
-    TextView mBtn3;
-    TextView mBtn4;
-    TextView mBtn5;
-    TextView mBtn6;
+    HorizontalProgressView mHealthBar;
+    ImageView mEnemyImage;
+    ImageView mHeroImage;
 
     InterstitialAd mInterstitialAd;
     RewardedVideoAd mRewardedVideoAd;
 
-     AppLovinAd loadedAd;
+    AppLovinAd loadedAd;
 
     AdColonyInterstitial mAdColonyInterstitial;
-
 
     boolean adColonyLoaded = false;
 
@@ -53,99 +44,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_room);
         mContext = getApplication().getApplicationContext();
 
-        mBtn1 = findViewById(R.id.btn_1);
-        mBtn2 = findViewById(R.id.btn_2);
-        mBtn3 = findViewById(R.id.btn_3);
-        mBtn4 = findViewById(R.id.btn_4);
-        mBtn5 = findViewById(R.id.btn_5);
-        mBtn6 = findViewById(R.id.btn_6);
+        mHealthBar = findViewById(R.id.health_bar);
+        mEnemyImage = findViewById(R.id.enemy_image);
+        mHeroImage = findViewById(R.id.hero_image);
 
-        //Show Google Interstitials
-        mBtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
-            }
-        });
-        //Show Google Rewarded Video
-        mBtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mRewardedVideoAd.isLoaded()) {
-                    mRewardedVideoAd.show();
-                }
-            }
-        });
-        mBtn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AppLovinInterstitialAdDialog interstitialAd = AppLovinInterstitialAd.create( AppLovinSdk.getInstance(mContext), mContext);
+        //Use this to load image into the ImageViews.
+        Glide.with(mContext).asDrawable().load(getDrawable(R.drawable.bg_rectangle_green_solid)).into(mEnemyImage);
 
-                // Optional: Assign listeners
-                interstitialAd.setAdDisplayListener(new AppLovinAdDisplayListener() {
-                    @Override
-                    public void adDisplayed(AppLovinAd appLovinAd) {
-
-                    }
-
-                    @Override
-                    public void adHidden(AppLovinAd appLovinAd) {
-                        loadAppLovinVideo();
-                    }
-                });
-                interstitialAd.setAdClickListener(new AppLovinAdClickListener() {
-                    @Override
-                    public void adClicked(AppLovinAd appLovinAd) {
-
-                    }
-                });
-                interstitialAd.setAdVideoPlaybackListener(new AppLovinAdVideoPlaybackListener() {
-                    @Override
-                    public void videoPlaybackBegan(AppLovinAd appLovinAd) {
-
-                    }
-
-                    @Override
-                    public void videoPlaybackEnded(AppLovinAd appLovinAd, double v, boolean b) {
-
-                    }
-                });
-
-                interstitialAd.showAndRender(loadedAd);
-            }
-        });
-        mBtn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        mBtn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (adColonyLoaded)
-                {
-                    mAdColonyInterstitial.show();
-                }
-                else
-                {
-                    loadAdColonyVideo();
-                }
-            }
-        });
-        mBtn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        //Use this to set Progress Bar.
+        mHealthBar.setEndProgress(80);
+        mHealthBar.startProgressAnimation();
 
         //Google Interstitials Initialization
         MobileAds.initialize(mContext, "ca-app-pub-3940256099942544~3347511713");
@@ -158,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 // Load the next interstitial.
                 mInterstitialAd.loadAd(new AdRequest.Builder().build());
             }
-
         });
 
         //Google Rewarded Video Initialization
